c3008ff5d81c3314288881a7a15fe16570fc7633
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-02-12 16:29:14 +0100

    initial commit

---
+++ quintorcatalog-backend/src/test/java/quintor/bioinf/nl/quintorcatalogbackend/QuintorcatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package quintor.bioinf.nl.quintorcatalogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuintorcatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
