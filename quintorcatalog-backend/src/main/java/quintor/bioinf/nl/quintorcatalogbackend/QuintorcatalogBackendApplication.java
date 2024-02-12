12573a19bee42a1fb17c25332b857493c8c5c4f8
MarkStreek <m.van.de.streek@st.hanze.nl>
2024-02-12 16:40:21 +0100

    initial

---
+++ quintorcatalog-backend/src/main/java/quintor/bioinf/nl/quintorcatalogbackend/QuintorcatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package quintor.bioinf.nl.quintorcatalogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintorcatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintorcatalogBackendApplication.class, args);
+	}
+
+}
